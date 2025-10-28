b000b9febe0300c03d6352ffd4cf799bcba7721b
moblando <landorams1@gmail.com>
2025-10-28 13:14:50 -0500

    Added compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,30 @@
-
-// An example gradle build file for a project that depends on the KSL
-
 plugins {
-    `java-library`
     kotlin("jvm") version "2.2.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
+    id("org.jetbrains.compose") version "1.7.0-alpha03"
 }
 
 repositories {
-
     mavenCentral()
+    google()
 }
 
-dependencies {
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
-    // next line allows use of KSL libraries within the project
-    // update the release number when new releases become available
-    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.5")
-  //  api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.0")
+dependencies {
+    implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
-    implementation(kotlin("stdlib-jdk8"))
+    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.5")
+}
+
+compose.desktop {
+    application {
+        mainClass = "Main.kt"
+    }
 }
 
 kotlin {
     jvmToolchain(21)
 }
+
